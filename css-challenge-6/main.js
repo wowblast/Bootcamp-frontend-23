30bd05eeb111cd6cede5fe0fdb53efb2054d0369
wowblast <jfrt142012@hotmail.com>
2023-01-15 22:32:09 -0400

    Add homework

---
+++ css-challenge-6/main.js
@@ -0,0 +1,11 @@
+function emailDirecctionHelp() {
+  document.getElementById('helpText').innerHTML = "put your personal email for the application";
+}
+
+function emailDescriptionHelp() {
+    document.getElementById('helpText').innerHTML = "Put a brieft description of why you want to apply";
+  }
+
+const helpText = ""
+document.getElementById('helpText').innerHTML = helpText;
+
