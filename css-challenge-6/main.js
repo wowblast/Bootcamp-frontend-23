a76461bfaec70f8a68d68437cecfdb73e92e2d32
wowblast <jfrt142012@hotmail.com>
2023-01-17 10:22:22 -0400

    Change color

--- css-challenge-6/main.js
+++ css-challenge-6/main.js
@@ -1,5 +1,5 @@
 function emailDirecctionHelp() {
-  document.getElementById('helpText').innerHTML = "put your personal email for the application";
+  document.getElementById('helpText').innerHTML = "put your personal email, an example can be 'name@example.com', you should check your email after you complete the form of the application ";
 }
 
 function emailDescriptionHelp() {
@@ -9,3 +9,8 @@ function emailDescriptionHelp() {
 const helpText = ""
 document.getElementById('helpText').innerHTML = helpText;
 
+
+function buttonHelp() {
+  document.getElementById('helpText').innerHTML = "after you send your application a confirmation will be sent to your email";
+
+}
