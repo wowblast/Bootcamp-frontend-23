fe7ef24d83b9b871eed9b286e6a5250c28965914
wowblast <jfrt142012@hotmail.com>
2023-01-26 12:35:14 -0400

    Add challenge 3 ts

---
+++ ts-challenge-3/node_modules/generic-logger-typings/dist/interfaces/levels.d.ts
@@ -0,0 +1,9 @@
+export declare enum LogLevel {
+    TRACE = 0,
+    DEBUG = 1,
+    INFO = 2,
+    WARN = 3,
+    ERROR = 4,
+    FATAL = 5,
+    OFF = 6
+}
---
+++ ts-challenge-3/pokemon.ts
@@ -0,0 +1,96 @@
+import fetch from "node-fetch";
+
+function checkPowerPoint(target: Object, propertyKey: string, descriptor: any) {
+  /*
+  
+  console.log("method", propertyKey)
+  console.log("descriptor ", descriptor)
+  //Wconsole.log(ar)*/
+  const originalMethod = descriptor.value;
+
+  descriptor.value = function (...args: any[]) {
+    if (args[0].power < 20) {
+      console.log("cant attack");
+    } else {
+      originalMethod.call(this, ...args);
+    }
+  };
+  return descriptor;
+}
+
+class Pokemon {
+  name: string;
+  ppAvailable = 1;
+  constructor(name: string, ppAvailable: number) {
+    this.name = name;
+    this.ppAvailable = ppAvailable;
+  }
+
+  @checkPowerPoint
+  figth(move: any) {
+    console.log(`${this.name} used ${move?.name}!`);
+    this.ppAvailable -= 1;
+  }
+}
+
+const move = { name: "thunderbolt", power: 10 };
+const move2 = { name: "thunderbolt", power: 90 };
+
+const pikachu = new Pokemon("pikachu", 1);
+//pikachu.figth(move);
+//pikachu.figth(move2);
+type Consturctor = { new (...args: any[]): any };
+
+function getPokemonsId(pokemons: number) {
+  let pokemonsIDs: number[] = [];
+  for (let index = 0; index < pokemons; index++) {
+    pokemonsIDs.push(Math.round(Math.random() * 1000));
+  }
+
+  return <T extends Consturctor>(BaseClass: T) => {
+    console.log(BaseClass);
+    return class extends BaseClass {
+      pokemons = pokemonsIDs;
+    };
+  };
+}
+
+@getPokemonsId(2)
+class Trainer {
+  name: string;
+  pokemons: number[];
+  pokemonsAmount: number = 0;
+  constructor(name: string, pokemonsAmount: number) {
+    this.name = name;
+    this.pokemonsAmount = pokemonsAmount;
+    //console.log("tiene "+ pokemonsAmount)
+  }
+  async getPokemons() {
+    console.log("tiene " + this.pokemonsAmount);
+    console.log(this.pokemons);
+    this.pokemons.forEach(async pokemon => {
+      console.log('https://pokeapi.co/api/v2/pokemon/'+ pokemon)
+      fetch('https://pokeapi.co/api/v2/pokemon/'+ pokemon, {})
+    .then((response) => response.text())
+    .then((body) => {
+      const data = JSON.parse(body)
+        console.log(this.name + ' has '+data.forms[0].name);
+        const move = { name: data.moves[0].move.name, power: 90 };
+
+        const newPokemon = new Pokemon(data.forms[0].name, 1);
+        newPokemon.figth(move);
+
+    });
+      
+    });
+  
+
+     
+  }
+}
+
+new Trainer('james',1).getPokemons();
+
+//;
+
+//pikachu.figth(move);
