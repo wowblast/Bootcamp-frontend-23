edc2279c649422166159ac05dc4c2ad9d876b8cd
wowblast <jfrt142012@hotmail.com>
2023-02-13 10:13:18 -0400

    add basic files

---
+++ angularProyect/pokeAPI/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.componet';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToDoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
