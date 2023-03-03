dec75452f8955f231a6956fca8ff16085908d092
wowblast <jfrt142012@hotmail.com>
2023-03-03 01:06:51 -0400

    button navigate on profile

--- angularProyect/pokeAPI/src/app/pokemon-profile-module/profie-view.module.ts
+++ angularProyect/pokeAPI/src/app/pokemon-profile-module/profie-view.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
+import { SharedModule } from "../shared/shared.module";
 import { PokemonRoutingModule } from "./profile-routing.module";
 import { ProfileViewComponent } from "./profile-view-component/profile-view.component";
 
 export const modules = [
-    PokemonRoutingModule
+    PokemonRoutingModule,
+    SharedModule
   ];
   
   
--- angularProyect/pokeAPI/src/app/pokemon-profile-module/profile-view-component/profile-view.component.ts
+++ angularProyect/pokeAPI/src/app/pokemon-profile-module/profile-view-component/profile-view.component.ts
@@ -1,18 +1,46 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { pokemonColorMap } from 'src/app/shared/config/pokemonColorHash';
 @Component({
-    selector: 'profile-view-component',
-    templateUrl: './profile-view.component.html',
-    styleUrls: ['./profile-view.component.css']
-
+  selector: 'profile-view-component',
+  templateUrl: './profile-view.component.html',
+  styleUrls: ['./profile-view.component.css'],
 })
 export class ProfileViewComponent implements OnInit {
- constructor( private route: ActivatedRoute) {
+  pokemonImage: string = '';
+  backgroundColor: string = '';
+  previousPokemon: string = '';
+  nextPokemon: string = '';
+  id = 1;
+  isPrevious = false;
+  constructor(private route: ActivatedRoute, private router: Router) {}
+  ngOnInit(): void {
+    this.setProfileData();
+  }
 
- }
-    ngOnInit(): void {
-        console.log("prfile", this.route.snapshot.data?.['profile'])
+  loadNextPokemon(diff: number) {
+    console.log(diff + this.id);
+    const newId = diff + this.id;
+    this.router.navigateByUrl('pokemons/profile/' + newId);
+    //this.setProfileData();
+  }
 
-    }
+  setProfileData() {
+    console.log('init data');
+    this.route.data.subscribe((data) => {
+        const profile = data['profile']
+      console.log('suscribe', profile);
+      this.backgroundColor =
+        pokemonColorMap[profile['id']];
+      //console.log('prfile', this.route.snapshot.data?.['profile'].id);
+      this.pokemonImage =
+      profile['sprites'].back_default;
+      this.id = parseInt(profile['id']);
 
+      this.previousPokemon = '###' + (this.id - 1);
+      this.nextPokemon = '###' + (this.id + 1);
+
+      if (this.id - 1 > 0) this.isPrevious = true;
+    });
+  }
 }
